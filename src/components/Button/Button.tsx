18df3db22e7cf3daa3652c8cb60b8d65c1f197c4
Jessy Avalos Barrales <jessyavalos16@outlook.es>
2023-01-14 19:26:58 -0600

    styled-component and react router added

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { ButtonProps } from "./ButtonTypes";
+import { BaseButton } from "./Style";
+
+const Button = ({ children, onClick }: ButtonProps): JSX.Element => {
+    return (
+        <BaseButton onClick={onClick}>
+            {children}
+        </BaseButton>
+    );
+}
+
+export default Button;
