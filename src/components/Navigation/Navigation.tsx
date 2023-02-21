b3d0340c1093e14c8c4a7288176554088d350256
Jessy Avalos Barrales <jessyavalos16@outlook.es>
2023-02-20 20:44:17 -0600

    Navigation added

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,10 +1,50 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import { NavigationContainer, NavText } from './Styles';
+
+interface routesProps {
+  path: string;
+  text: string;
+}
 
 const Navigation = (): JSX.Element => {
-    return (
-        <>
-        </>
-    );
-}
+  const routes: routesProps[] = [
+    {
+      path: '/',
+      text: 'Jessy',
+    },
+    {
+      path: '/',
+      text: 'Home',
+    },
+    {
+      path: '/portfolio',
+      text: 'Portfolio',
+    },
+    {
+      path: '/experience',
+      text: 'Experience',
+    },
+    {
+      path: '/about',
+      text: 'About',
+    },
+    {
+      path: '/contact',
+      text: 'avalosbarralesjessy@gmail.com',
+    },
+  ];
+
+  return (
+    <NavigationContainer>
+      {routes.map(({ path, text }: routesProps, index: number) => (
+        <NavText key={index} to={path}>
+          {text}
+        </NavText>
+      ))}
+    </NavigationContainer>
+  );
+};
 
 export default Navigation;
---
+++ src/theme/Theme.ts
@@ -0,0 +1,23 @@
+import { DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
+  colors: {
+    black: '#000000',
+    white: '#FFFFFF',
+    body_text: '#777990',
+    primaryColor: '#37545E',
+    primaryColor_light: '#5295AB',
+    secondaryColor: '#5E4B37',
+    secondaryColor_light: '#AB7641',
+  },
+  fonts: {
+    bold: 'Roboto-Bold',
+    medium: 'Roboto-Medium',
+    italic: 'Roboto-Italic',
+    light: 'Roboto-Light',
+    thin: 'Roboto-Thin',
+    regular: 'Roboto-Regular',
+  },
+};
+
+export default theme;
